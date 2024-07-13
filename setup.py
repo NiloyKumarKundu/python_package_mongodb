ecb024f53d91157de13e2fd0a6eb9d3d3b861476
NiloyKumarKundu <nkundu191071@bscse.uiu.ac.bd>
2024-07-13 16:35:25 +0600

    modified files

--- setup.py
+++ setup.py
@@ -2,24 +2,24 @@ from setuptools import setup, find_packages
 from typing import List
 
 
-HYPEN_E_DOT = '-e .'
+# HYPEN_E_DOT = '-e .'
 
-def get_requirement(file_path:str) -> List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements = f.readlines()
-        requirements = [req.replace('\n', '') for req in requirements]
+# def get_requirement(file_path:str) -> List[str]:
+#     requirements = []
+#     with open(file_path) as f:
+#         requirements = f.readlines()
+#         requirements = [req.replace('\n', '') for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements
+#         if HYPEN_E_DOT in requirements:
+#             requirements.remove(HYPEN_E_DOT)
+#     return requirements
 
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
 
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 REPO_NAME = "python_package_mongodb"
 PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "NiloyKumarKundu"
@@ -39,5 +39,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_require=get_requirement('./requirements_dev.txt')
+    # install_require=get_requirement('./requirements_dev.txt')
 )
